1479f33cfb3d8d123be8fda6903893cf7069218b
Shakthi <56283616+shakthiasu@users.noreply.github.com>
2019-11-01 20:54:57 -0700

    Serializable implementation
    Structure food class to display details with strings

--- app/src/main/java/com/example/fooddatabase/model/Food.java
+++ app/src/main/java/com/example/fooddatabase/model/Food.java
@@ -1,9 +1,10 @@
 package com.example.fooddatabase.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Food {
+public class Food implements Serializable {
 
     String label;
     String carbs;
